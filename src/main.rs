bdc534a7e4dc9f7be6e177dab6c589e5a3fb5129
Juyeong Maing <mjy9088@naver.com>
2024-03-03 03:52:38 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    let vec = line
+        .trim()
+        .split(' ')
+        .flat_map(|x| x.parse::<i32>())
+        .collect::<Vec<_>>();
+
+    println!("{}", vec[0] - vec[1]);
+}
